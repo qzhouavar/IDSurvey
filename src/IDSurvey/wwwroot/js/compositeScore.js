6e08f4354f6aefa04cf727f29e5ba9a915cc70c9
sgavar <sgou@avarconsulting.com>
2016-12-02 10:58:49 -0500

    temp changes

--- src/IDSurvey/wwwroot/js/compositeScore.js
+++ src/IDSurvey/wwwroot/js/compositeScore.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
         if ($.fn.DataTable.isDataTable(eleid)) {
             $(eleid).DataTable().destroy();
         }
+        
         $(eleid).DataTable({
             data: data,
             columns: [
@@ -78,9 +79,9 @@ $(document).ready(function () {
         $(document).ajaxStart();
         //get current select option
         var quarter = $('#select-quarter').val().toString();
-        var targeturl = encodeURI("/GetCompositeScoreByArea/" + quarter);
+        var compositeUrl = encodeURI("/GetCompositeScoreByArea/" + quarter);
         var overallUrl = encodeURI("/GetOverallRatingByArea/" + quarter);
-        getData(targeturl, function (d) {
+        getData(compositeUrl, function (d) {
             createCompositeDataTable('#allResult', d['ALL']);
             createCompositeDataTable('#appealResult', d['APPEALS']);
             createCompositeDataTable('#complaintResult', d['COMPLAINTS']);
@@ -89,6 +90,24 @@ $(document).ready(function () {
             createOverallDataTable('#overallRating', d);
         });
         $('#current-quarter').text('Quarter: ' + $('#select-quarter option:selected').text());
+        $(document).ajaxStop();
+    });
+
+    $('#select-month-btn').click(function () {
+        $(document).ajaxStart();
+        //get current select option
+        var quarter = $('#select-month').val().toString();
+        var compositeUrl = encodeURI("/GetCompositeScoreByArea/" + quarter);
+        var overallUrl = encodeURI("/GetOverallRatingByArea/" + quarter);
+        getData(compositeUrl, function (d) {
+            createCompositeDataTable('#allResult', d['ALL']);
+            createCompositeDataTable('#appealResult', d['APPEALS']);
+            createCompositeDataTable('#complaintResult', d['COMPLAINTS']);
+        });
+        getData(overallUrl, function (d) {
+            createOverallDataTable('#overallRating', d);
+        });
+        $('#current-quarter').text('Quarter: ' + $('#select-month option:selected').text());
         $(document).ajaxStop();
     });
 
