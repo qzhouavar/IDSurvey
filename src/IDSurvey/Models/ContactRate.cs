8376a87f96a5aa4060a0eb12271f06b1a6591148
qzhouavar <qzhou@avarconsulting.com>
2016-12-15 17:34:46 -0500

    Add contact rate

---
+++ src/IDSurvey/Models/ContactRate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDSurvey.Models
+{
+    public class ContactRate
+    {
+        [Key]
+        public string QTR { get; set; }
+      
+        [Required]
+        public int TOTAL { get; set; }
+
+        [Required]
+        public int COMPLETED { get; set; }
+
+
+    }
+}
