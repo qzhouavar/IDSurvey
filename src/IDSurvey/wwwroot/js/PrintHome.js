7e7a09aafdded49021dfc148b9be6e8a685085af
qzhouavar <qzhou@avarconsulting.com>
2016-12-20 12:03:43 -0500

    change print format

---
+++ src/IDSurvey/wwwroot/js/PrintHome.js
@@ -0,0 +1,37 @@
+﻿(function () {
+            var beforePrint = function () {
+                for (var i = 0; i < AmCharts.charts.length-3; i++) {
+                    var chart = AmCharts.charts[i];
+                    chart.div.style.width = "1000px";
+                    chart.validateNow();
+                }
+                for (var i = AmCharts.charts.length - 3; i < AmCharts.charts.length; i++) {
+                    var chart = AmCharts.charts[i];
+                    chart.div.style.width = "400px";
+                    chart.validateNow();
+                }
+
+            };
+
+            var afterPrint = function () {
+                for (var i = 0; i < AmCharts.charts.length; i++) {
+                    var chart = AmCharts.charts[i];
+                    chart.div.style.width = "100%";
+                    chart.validateNow();
+                }
+            };
+
+            if (window.matchMedia) {
+                var mediaQueryList = window.matchMedia('print');
+                mediaQueryList.addListener(function (mql) {
+                    if (mql.matches) {
+                        beforePrint();
+                    } else {
+                        afterPrint();
+                    }
+                });
+            }
+
+            window.onbeforeprint = beforePrint;
+            window.onafterprint = afterPrint();
+        }());
